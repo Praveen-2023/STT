86126570943acfe148419318a96c5e21ce0edf89
Praveen-2023 <143311712+Praveen-2023@users.noreply.github.com>
2025-01-09 10:53:39 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+#defining the variables
 A = 5
 B = 8
 C = 10
@@ -9,21 +10,20 @@ if A > B:
     print(A, "is greater than" ,B)
 else:
     print(B, "is grater than " ,A)
+    
 print("sum of a,b,c  is :",X)
 print("Sub of c-b  is :",Y)
 print("multiplication of a,b,c  is :",Z)
 print("division of c/a is :",W)
 arr = [1,4,6,3,74,35,23,76,45]
 print (arr)
-MAX = 0
-for index, NUM in enumerate(arr):
-    MAX = max(MAX,NUM)
+
+MAX = max(MAX,NUM)
 print ("laregest number in the array is :",MAX)
-SMALL = arr[0]
-for index, NUM in enumerate(arr):
-    SMALL = min(SMALL,NUM)
+
+SMALL = min(SMALL,NUM)
 print ("Smallest number in the array is :",SMALL)
-TOTAL=0
+
 for index, NUM in enumerate(arr):
     TOTAL += NUM
 AVG = TOTAL/len(arr)
